0639e9939e47d02a3cfa9933873e99c710a24d93
Leeco <leeco19@qq.com>
2023-06-07 15:44:58 +0800

    chore: hello foundry

--- src/Counter.sol
+++ src/Counter.sol
@@ -11,4 +11,8 @@ contract Counter {
     function increment() public {
         number++;
     }
+
+    function double() public {
+        number *= 2;
+    }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,15 +1,24 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
 import "../src/Counter.sol";
+import "forge-std/Test.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+    address public alice;
+    Helper public h;
+    IERC20 public dai;
 
     function setUp() public {
         counter = new Counter();
         counter.setNumber(0);
+
+        h = new Helper();
+
+        alice = address(1);
+        dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     }
 
     function testIncrement() public {
@@ -20,5 +29,57 @@ contract CounterTest is Test {
     function testSetNumber(uint256 x) public {
         counter.setNumber(x);
         assertEq(counter.number(), x);
+    }
+
+    function testCheatCode() public {
+        // vm 用来控制 evm 状态
+        console2.log("Cheat code activated!");
+        emit log_address(address(1));
+        console2.log("before: ", block.timestamp);
+        vm.warp(1000);
+        console2.log("after: ", block.timestamp);
+        console2.log(address(this));
+
+        vm.prank(alice); // 只影响一次
+        address caller = h.whoCalled();
+        console2.log(caller);
+
+        address caller1 = h.whoCalled();
+        console2.log(caller1);
+
+        // vm.starPrank(alice);
+        // // 区间内都会受影响...
+        // vm.stopPrank();
+
+        console2.log("before: ", alice.balance);
+        vm.deal(alice, 1 ether); // 改变账户状态
+        console2.log("after: ", alice.balance);
+
+        // vm.envAddress("DAI"); // 从环境变量读取值
+        string memory rpc = vm.envString("ETH_RPC_URL");
+        uint256 mainnet = vm.createFork(rpc);
+        vm.selectFork(mainnet);
+        // 下面的命令都在 mainnet 网络上执行
+        console2.log("before: ", dai.balanceOf(alice));
+        deal(address(dai), alice, 1 ether);
+        console2.log("after: ", dai.balanceOf(alice));
+
+        string memory message = "abc";
+        bytes32 hash = keccak256(abi.encodePacked(message));
+        console2.logBytes32(hash);
+
+        string[] memory cmd = new string[](3);
+        cmd[0] = "cast";
+        cmd[1] = "keccak";
+        cmd[2] = message;
+        bytes memory result = vm.ffi(cmd);
+        bytes32 hash1 = abi.decode(result, (bytes32));
+        assertEq(hash, hash1);
+    }
+}
+
+contract Helper {
+    function whoCalled() public returns (address) {
+        return msg.sender;
     }
 }
