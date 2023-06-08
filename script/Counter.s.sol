c0bd1a3b5c0be6249b39bd3586dd210dae47a82d
Leeco <leeco19@qq.com>
2023-06-09 00:25:32 +0800

    feat: p3 deploy

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {
@@ -9,8 +10,14 @@ contract CounterScript is Script {
     }
 
     function run() public {
-        vm.broadcast();
+        // vm.broadcast();
         console2.log("CounterScript run");
+        // 开始记录脚本中合约的调用和创建
+        vm.startBroadcast();
+        //生成合约对象
+        Counter c = new Counter();
+        // 结束记录
+        vm.stopBroadcast();
     }
 
     function someFunction(uint256 x) public {
@@ -23,9 +30,8 @@ contract CounterScript is Script {
         console2.log(x);
     }
 
-    function someFunction3(bytes32 x) public {
+    function someFunction3(bytes calldata x) public {
         console2.log("CounterScript someFuntion3");
-        bytes memory y = 'hello';
-        console2.log(string(y));
+        console2.log(string(x));
     }
 }
