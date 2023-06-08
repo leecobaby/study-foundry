af232274a6d0e16df6cc805039234130b622dfd9
Leeco <leeco19@qq.com>
2023-06-08 20:13:49 +0800

    feat: p2 script

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -4,9 +4,28 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 
 contract CounterScript is Script {
-    function setUp() public {}
+    function setUp() public {
+        console2.log("CounterScript setUp");
+    }
 
     function run() public {
         vm.broadcast();
+        console2.log("CounterScript run");
+    }
+
+    function someFunction(uint256 x) public {
+        console2.log("CounterScript someFuntion");
+        console2.log(x);
+    }
+
+    function someFunction2(string calldata x) public {
+        console2.log("CounterScript someFuntion2");
+        console2.log(x);
+    }
+
+    function someFunction3(bytes32 x) public {
+        console2.log("CounterScript someFuntion3");
+        bytes memory y = 'hello';
+        console2.log(string(y));
     }
 }
